62300f1410cf5a4549513a0d219c51f39cf2c6d8
Edgar Lemus <elemus@todo1.net>
2019-12-23 20:47:53 +0000

    KarateRunner.java editado, se elimina el comentario

--- src/test/java/runners/KarateRunner.java
+++ src/test/java/runners/KarateRunner.java
@@ -5,8 +5,6 @@ import com.intuit.karate.junit4.Karate;
 import org.junit.runner.RunWith;
 
 @RunWith(Karate.class)
-@KarateOptions(features = "src\\test\\resources\\features\\karate.feature"
-//,tags = "@PutCompleto"
-)
+@KarateOptions(features = "src\\test\\resources\\features\\karate.feature",tags = "@PutCompleto")
 public class KarateRunner {
 }
